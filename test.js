1a2a0fd8dfdb5b5e0e2f4afa9292b900bd1a5cd8
Rohit <rohitsasi720@gmail.com>
2023-12-05 14:45:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+  console.log("testttt");
   let targetElement = document.querySelector('body');
   let messageElement = document.createElement('div');
   messageElement.textContent = 'Hello, Rohit!';
